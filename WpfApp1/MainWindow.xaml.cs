bcf30b8b933923a5bb453537e9c53dad95fd52c8
Noah Imamkhan <s1129092@student.windesheim.nl>
2020-02-15 19:49:35 +0100

    First

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    ///
+    ///f
     ///
     // class that acts like a session
     // if it's null we do not allow them to login
@@ -38,7 +38,7 @@ namespace WpfApp1
             }
 
             // create new restclient class, with the base url filled in the ()
-            var client = new RestClient("http://scrum.clow.nl/");
+            var client = new RestClient("apage");
 
             //  create new restrequest class, withe the rest of the url we want filled in
             var request = new RestRequest("api/project");
@@ -51,7 +51,7 @@ namespace WpfApp1
 
             // add the default header with authorization needed
             client.AddDefaultHeader("Authorization",
-                string.Format("Bearer {0}", "0ecda346-571d-3dfc-8c8c-c0648cce87d7"));
+                string.Format("Bearer {0}", "TOKEN"));
 
             // we execute and get the content
             var response = client.Execute(request);
@@ -67,51 +67,6 @@ namespace WpfApp1
             
         }
 
-        private void login_click_1(object sender, RoutedEventArgs e)
-        {
-            // create new restclient class, with the base url filled in the ()
-            var client = new RestClient("http://scrum.clow.nl/");
-
-            //  create new restrequest class, withe the rest of the url we want filled in
-            var request = new RestRequest("api/auth/signin");
-
-            request.RequestFormat = DataFormat.Json;
-            request.AddBody(new {email = "s1129092@student.windesheim.nl", password = "jojo1234"});
-
-            // we define the method
-            request.Method = Method.POST;
-
-            // add the header Accept
-            request.AddHeader("Accept", "application/json");
-
-            // we execute and get the content
-            var response = client.Execute(request);
-
-
-            if (response.StatusCode.ToString() == "OK")
-            {
-                MessageBox.Show("User is allowed to login");
-
-                Application.Current.Properties["ID"] = 1;
-
-                int id = int.Parse(Application.Current.Properties["ID"].ToString());
-
-
-            } else if (response.StatusCode.ToString() == "422")
-            {
-                MessageBox.Show("Incorrect login details");
-            }
-            else
-            {
-                MessageBox.Show("ERROR");
-            }
-        }
-
-        private void logout_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Properties.Remove("ID");
-        }
-
 
     }
 
@@ -125,8 +80,6 @@ namespace WpfApp1
         public int User_id { get; set; }
         public string Created_at { get; set; }
         public string Updated_at { get; set; }
-
-
     }
 
     class ProjectList
